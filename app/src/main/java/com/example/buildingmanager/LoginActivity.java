2b7fc8df0b25a3a32467f119e7ff8c898a2f74ff
RattleTotem <domen.ramsak@gmail.com>
2019-12-12 17:57:35 +0100

    Register

--- app/src/main/java/com/example/buildingmanager/LoginActivity.java
+++ app/src/main/java/com/example/buildingmanager/LoginActivity.java
@@ -2,13 +2,33 @@ package com.example.buildingmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.usernameField);
+        username = findViewById(R.id.passwordField);
+        login = findViewById(R.id.loginButton);
+
     }
+
+    public void next(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
 }
