5d18a12d068d5d0e830e455cb5f3ec768acb4aba
RattleTotem <domen.ramsak@gmail.com>
2020-01-11 19:05:15 +0100

    drawer

--- app/src/main/java/com/example/buildingmanager/MainActivity.java
+++ app/src/main/java/com/example/buildingmanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.buildingmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,15 +9,22 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     FirebaseAuth auth;
     private DrawerLayout drawer;
+    private TextView navNameField;
+    private TextView navEmailField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +37,51 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navNameField = findViewById(R.id.nav_name);
+        navEmailField = findViewById(R.id.nav_email);
+
+
+        try {
+            navEmailField.setText(auth.getCurrentUser().getEmail());
+        } catch(Exception e) {
+                Log.e("ERROR", e.getMessage() );
+            e.getMessage();
+        }
+
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
     }
 
-    public void signOut(View view) {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.nav_share:
+                signOut();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void signOut() {
         auth.signOut();
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
